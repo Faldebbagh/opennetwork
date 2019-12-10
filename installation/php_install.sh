58b3814cdde07d9fe52996b5ebea2c9d8a6f7698
Faldebbagh <58316629+Faldebbagh@users.noreply.github.com>
2019-12-11 00:28:15 +0100

    Create php_install.sh
    install per web skript

---
+++ installation/php_install.sh
@@ -0,0 +1,41 @@
+install_start(){
+  red=`tput setaf 1`
+  green=`tput setaf 2`
+  reset=`tput sgr0`
+  echo vorbereitung auf webinterface install
+  echo Bitte warten !!!!!!!!!
+  echo Installiere apache2 Web-Servie\nfür das Webpanel
+#  sudo apt install apache2 -y
+  sleep 1
+  echo Installiere PHP7
+#  sudo apt install php -y
+  clear
+  cp -r ./installation/html  /var/www/html_test
+ 	if [ -d "/etc/opennetwork" ];then
+	  cp -r ./ /etc/opennetwork
+	else
+	mkdir /etc/opennetwork
+	cp -r ./ /etc/opennetwork
+  	fi
+	sleep 1
+  echo "chek if apache2 istalliert wurde !"
+  software_name="apache2"
+  software_chek
+  echo "chek if PHP istalliert wurde !"
+  software_name="php"
+  software_chek
+
+	ip=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+	 while true
+	     do
+	      if [ -f "/etc/opennetwork/install_php_protokoll.log" ];then
+	           echo "installation wurde abgeschlossen"
+             exit
+	          else
+  	          echo "${green}Gehen Sie bitte google chrome"
+  	           echo "http://$ip/install.php${reset} "
+                sleep 3
+                clear
+	      fi
+	done
+}
