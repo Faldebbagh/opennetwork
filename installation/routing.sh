a4af4d770ebc9cf3cd0439bc2d0fb669dd911902
Firas <58316629+Faldebbagh@users.noreply.github.com>
2019-12-15 12:23:14 +0100

    Update routing.sh

--- installation/routing.sh
+++ installation/routing.sh
@@ -1,4 +1,6 @@
 routing(){
+  case $benutzer_install_art in
+    [nN])
   echo "Routing und NAT für die Internet-Verbindung konfigurieren"
   echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
   sleep 4
@@ -21,4 +23,28 @@ routing(){
   clear
   echo "Die neu Routing-Information wurden gespeichert"
   sleep 4
+  ;;
+  [wW])
+  echo "Routing und NAT für die Internet-Verbindung konfigurieren" "<p></p>"
+  echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+  echo "Routing-Informationen werden beim Systemstart geladen" "<p></p>"
+  if [ -f "/etc/init.d/firewall.sh" ]; then
+    echo "alte Routing-Informationen wurde erkannt" "<p></p>"
+    sudo rm /etc/init.d/rules.sh
+    sudo update-rc.d -f rules.sh remove
+    sudo rm -r /etc/opennetwork/
+    echo "alte Routing-Informationen wurde entferrnt" "<p></p>"
+  fi
+  cp installation/rules /etc/init.d/rules.sh
+  chmod +x /etc/init.d/rules.sh
+  sudo mkdir /etc/opennetwork
+  cp -r  verwaltung /etc/opennetwork
+  sudo chmod 755 /etc/opennetwork
+  sudo update-rc.d rules.sh defaults
+  sudo echo "@reboot root /etc/init.d/rules.sh" >> /etc/crontab
+  echo "Die neu Routing-Information wurden gespeichert" "<p></p>"
+  ;;
+  *)
+esac
+
 }
