1b8f15c6b1d46260218fc83bbc0775484f90102a
Firas <58316629+Faldebbagh@users.noreply.github.com>
2019-12-22 02:15:27 +0100

    Update php_install.sh

--- installation/php_install.sh
+++ installation/php_install.sh
@@ -10,7 +10,7 @@ install_start(){
   echo Installiere PHP7
 #  sudo apt install php -y
   clear
-  echo "php ALL=(ALL) ALL" >> /etc/sudoers
+  echo "www-data ALL=(root:root) NOPASSWD:/bin/bash /etc/opennetwork/*.* " >> /etc/sudoers
   cp -r ./installation/html  /var/www/html
  	if [ -d "/etc/opennetwork" ];then
 	  cp -r ./ /etc/opennetwork
